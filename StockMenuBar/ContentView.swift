8ed53247788d0cf3175f853cf7b2d0d42fa2ab75
Tris <hbabnd@gmail.com>
2026-06-25 15:24:02 +0900

    Initial Commit

---
+++ StockMenuBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockMenuBar
+//
+//  Created by COMATOKI on 2026-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockMenuBar/StockMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockMenuBarApp.swift
+//  StockMenuBar
+//
+//  Created by COMATOKI on 2026-06-25.
+//
+
+import SwiftUI
+
+@main
+struct StockMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
